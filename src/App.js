264145c2c4eef37f7d4baa4f270862cfb5719188
Lave <laveshbisht01@gmail.com>
2023-03-14 15:13:04 +0530

    Merging Login and Register api's to out frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import { Auth } from './pages/auth';
+import { CreateRecipe } from './pages/create-recipe';
+import { Home } from './pages/home';
+import { Navbar } from './components/navbar';
+import { SavedRecipes } from './pages/saved-recipe';
+
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <Router>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/create-recipe" element={<CreateRecipe />} />
+        <Route path="/saved-recipes" element={<SavedRecipes />} />
+      </Routes>
+    </Router>
     </div>
-  );
+    );
 }
 
 export default App;
